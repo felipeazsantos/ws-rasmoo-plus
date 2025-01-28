4922776ad15ffb4836e86d72cf6e472d6e353bbe
Felipe Azevedo <felipe.azevedo@cws.digital>
2025-01-28 11:39:07 -0300

    ending create models

--- src/main/java/com/client/ws/rasmooplus/model/UserType.java
+++ src/main/java/com/client/ws/rasmooplus/model/UserType.java
@@ -1,17 +1,25 @@
 package com.client.ws.rasmooplus.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Entity
 @Table(name = "user_type")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserType implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_type_id")
-    private long id;
+    private Long id;
 
     private String name;
     private String description;
