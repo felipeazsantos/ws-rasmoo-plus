7db0f7461ec8f229f803dedaf9c76fcfb04762b0
Felipe Azevedo <felipe.azevedo@cws.digital>
2025-02-01 14:04:29 -0300

    treatment spring boot validation exceptions

--- src/main/java/com/client/ws/rasmooplus/exception/handler/ResourceHandler.java
+++ src/main/java/com/client/ws/rasmooplus/exception/handler/ResourceHandler.java
@@ -5,8 +5,14 @@ import com.client.ws.rasmooplus.exception.BadRequestException;
 import com.client.ws.rasmooplus.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ResourceHandler {
@@ -28,6 +34,26 @@ public class ResourceHandler {
                 HttpStatus.BAD_REQUEST,
                 HttpStatus.BAD_REQUEST.value()
         );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponseDto);
+    }
+
+    //MethodArgumentNotValidException
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDto> methodArgumentNotValidException(MethodArgumentNotValidException ex) {
+
+        Map<String, String> messages = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error ->  {
+            String field = ((FieldError) error).getField();
+            String defaultMessage = error.getDefaultMessage();
+            messages.put(field, defaultMessage);
+        });
+
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                Arrays.toString(messages.entrySet().toArray()),
+                HttpStatus.BAD_REQUEST,
+                HttpStatus.BAD_REQUEST.value()
+        );
+
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponseDto);
     }
 }
