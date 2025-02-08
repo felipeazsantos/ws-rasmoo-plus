dafb4e432317a3797028eff4753f4501ea76e9e8
Felipe Azevedo <felipe.azevedo@cws.digital>
2025-02-07 22:11:17 -0300

    connecting spring security with user from db

--- src/main/java/com/client/ws/rasmooplus/configuration/WebSecurityConfig.java
+++ src/main/java/com/client/ws/rasmooplus/configuration/WebSecurityConfig.java
@@ -1,23 +1,50 @@
 package com.client.ws.rasmooplus.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.
                 authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/subscription-type/**").permitAll()
-                        .anyRequest().authenticated()
-                ).build();
+                        .requestMatchers(HttpMethod.GET, "/subscription-type/**").permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(authProvider);
     }
 
 }
---
+++ src/main/java/com/client/ws/rasmooplus/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.client.ws.rasmooplus.service.impl;
+
+import com.client.ws.rasmooplus.exception.NotFoundException;
+import com.client.ws.rasmooplus.repository.UserDetailsRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserDetailsRepository userDetailsRepository;
+
+    public UserDetailsServiceImpl(UserDetailsRepository userDetailsRepository) {
+        this.userDetailsRepository = userDetailsRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       return userDetailsRepository.findByUsername(username)
+               .orElseThrow(() -> new NotFoundException("Usuario não encontrado: " + username));
+    }
+}
